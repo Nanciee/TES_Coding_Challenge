399dff959a3477a867ec9fb7508bf69d83af5c74
Nanciee <nancyduyi@gmail.com>
2017-01-10 20:19:38 -0500

    init

---
+++ client/app/app.js
@@ -0,0 +1,20 @@
+angular.module('TES', [
+  'ui.router'
+])
+.controller('TESController', function() {
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // var aboutState = {
+  //   name: 'about',
+  //   url: '/about',
+  //   templateUrl: 'app/about/about.html',
+  //   controller: 'AboutController',
+  //   controllerAs: 'AboutVm',
+  // };
+
+  // $stateProvider.state(aboutState);
+
+  // $urlRouterProvider.otherwise('/about');
+});
