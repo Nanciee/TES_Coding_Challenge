40dd2872a4f6aac964d49038518272238b37bd43
Nanciee <nancyduyi@gmail.com>
2017-01-11 15:03:21 -0500

    Add fontawesome icons for editing and deleting

--- client/app/manage/manage.js
+++ client/app/manage/manage.js
@@ -6,5 +6,6 @@ angular.module('TES.manage', [])
   services.getUsers().then(function(users){
     ManageVm.users = services.convertToJSON(users);
     ManageVm.users.shift();
+    ManageVm.users.pop();
   });
 });
