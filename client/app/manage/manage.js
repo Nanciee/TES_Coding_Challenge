7535ff8240c57068f09b9b6094ac033355295646
Nanciee <nancyduyi@gmail.com>
2017-01-11 16:29:28 -0500

    Add ability for sort in ASC or DES order for both first and last name

--- client/app/manage/manage.js
+++ client/app/manage/manage.js
@@ -3,20 +3,43 @@ angular.module('TES.manage', [])
   var ManageVm = this;
   ManageVm.users = [];
   ManageVm.clickedUser = {};
+  ManageVm.sortType = ['first_name', 'ASC']
+
+  ManageVm.editUser = function(user) {
+    ManageVm.clickedUser.clicked = false;
+    user.clicked = true;
+    ManageVm.clickedUser = user;
+  };
+
+  ManageVm.deleteUser = function(index) {
+    ManageVm.users.splice(index, 1);
+  };
+
+  ManageVm.sortUsers = function(title, order){
+    ManageVm.users = ManageVm.users.sort(function(a, b){
+      if(order === 'ASC'){
+        return a[title] > b[title] ? 1 : -1
+      } else if (order === 'DES'){
+        return a[title] > b[title] ? -1 : 1
+      }
+    });
+  };
+
+  ManageVm.changeSort = function(event){
+    var className = event.target.className
+    if(className === ManageVm.sortType[0]){
+      ManageVm.sortType[1] = ManageVm.sortType[1] === 'ASC' ? 'DES' : 'ASC';
+    } else {
+      ManageVm.sortType = [className, 'ASC'];
+    }
+
+    ManageVm.sortUsers(ManageVm.sortType[0], ManageVm.sortType[1]);
+  };
 
   services.getUsers().then(function(users){
     ManageVm.users = services.convertToJSON(users);
     ManageVm.users.shift();
     ManageVm.users.pop();
+    ManageVm.sortUsers('first_name', 'ASC');
   });
-
-  ManageVm.editUser = function(user) {
-    user.clicked = true;
-    ManageVm.clickedUser.clicked = false;
-    ManageVm.clickedUser = user;
-  }
-
-  ManageVm.deleteUser = function(index) {
-    ManageVm.users.splice(index, 1);
-  }
 });
--- spec/spec.js
+++ spec/spec.js
@@ -16,7 +16,7 @@ describe('TES', function () {
 
   }));
 
-  it('should have a users property on the ManageVm', function () {
+  it('should have an users property on the ManageVm', function () {
     expect(ManageVm.users).to.be.an('array');
   });
 
@@ -24,7 +24,7 @@ describe('TES', function () {
     expect(ManageVm.editUser).to.be.an('function');
   });
 
-  it('should have an deleteUser function on the ManageVm', function () {
+  it('should have a deleteUser function on the ManageVm', function () {
     expect(ManageVm.deleteUser).to.be.an('function');
   });
 
@@ -60,5 +60,91 @@ describe('TES', function () {
 
     ManageVm.deleteUser(1)
     expect(ManageVm.users).to.deep.equal(expectedResult);
+  });
+
+  it('should have a sortUsers function on the ManageVm', function () {
+    expect(ManageVm.sortUsers).to.be.an('function');
+  });
+
+  it('should sort user in ASC when passing `ASC` into `ManageVm.sortUsers`', function () {
+    ManageVm.users = [
+      {
+        first_name: 'Sandra',
+        last_name: 'Adams',
+        email: 'sadams2@barnesandnoble.com'
+      },
+      {
+        first_name: 'Matthew',
+        last_name: 'Watkins',
+        email: 'mwatkins0@google.com.hk'
+      },
+      {
+        first_name: 'Philip',
+        last_name: 'Myers',
+        email: 'pmyers1@qq.com'
+      }];
+
+    var expectedResult = [
+      {
+        first_name: 'Matthew',
+        last_name: 'Watkins',
+        email: 'mwatkins0@google.com.hk'
+      },
+      {
+        first_name: 'Philip',
+        last_name: 'Myers',
+        email: 'pmyers1@qq.com'
+      },
+      {
+        first_name: 'Sandra',
+        last_name: 'Adams',
+        email: 'sadams2@barnesandnoble.com'
+      }];
+
+    ManageVm.sortUsers('first_name', 'ASC')
+    expect(ManageVm.users).to.deep.equal(expectedResult);
+  });
+
+  it('should sort user in DES when passing `DES` into `ManageVm.sortUsers`', function () {
+    ManageVm.users = [
+      {
+        first_name: 'Sandra',
+        last_name: 'Adams',
+        email: 'sadams2@barnesandnoble.com'
+      },
+      {
+        first_name: 'Matthew',
+        last_name: 'Watkins',
+        email: 'mwatkins0@google.com.hk'
+      },
+      {
+        first_name: 'Philip',
+        last_name: 'Myers',
+        email: 'pmyers1@qq.com'
+      }];
+
+    var expectedResult = [
+      {
+        first_name: 'Sandra',
+        last_name: 'Adams',
+        email: 'sadams2@barnesandnoble.com'
+      },
+      {
+        first_name: 'Philip',
+        last_name: 'Myers',
+        email: 'pmyers1@qq.com'
+      },
+      {
+        first_name: 'Matthew',
+        last_name: 'Watkins',
+        email: 'mwatkins0@google.com.hk'
+      }];
+
+    ManageVm.sortUsers('first_name', 'DES')
+    expect(ManageVm.users).to.deep.equal(expectedResult);
+  });
+
+  it('should have a changeSort function on the ManageVm', function () {
+    expect(ManageVm.changeSort).to.be.an('function');
   });
 });
