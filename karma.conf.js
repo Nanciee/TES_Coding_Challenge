5c3298e217ec85aea6c76cf2e1b01b633374dac8
Nanciee <nancyduyi@gmail.com>
2017-01-10 23:39:06 -0500

    Add testing environment

---
+++ karma.conf.js
@@ -0,0 +1,39 @@
+// Karma configuration
+
+module.exports = function (config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: './',
+
+    // testing frameworks to use
+    frameworks: ['mocha', 'chai', 'sinon'],
+
+    // list of files / patterns to load in the browser. order matters!
+    files: [
+      // angular source
+      'client/bower_components/angular/angular.js',
+      'client/bower_components/angular-route/angular-route.js',
+      'client/bower_components/angular-mocks/angular-mocks.js',
+
+      // app code
+      'client/app/**/*.js',
+
+      // spec files
+      'specs/TESSpec.js',
+    ],
+
+    // test results reporter to use
+    reporters: ['nyan', 'unicorn'],
+
+    // start these browsers. PhantomJS will load up in the background
+    browsers: ['PhantomJS'],
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // if true, Karma exits after running the tests.
+    singleRun: true
+
+  });
+};
